08bacca6e5e8f76ed8fcc0f56ee8c8a586a87e62
Dineshkumar <dineshwatto123@gmail.com>
2025-10-28 20:41:12 +0530

    login form

---
+++ loginapi/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password
+
+# Create your models here.
+
+class UserRegister(models.Model):
+
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES)
+
+
+    def save(self, *args, **kwargs):
+        # Hash the password before saving (only if it’s not already hashed)
+        if not self.password.startswith('pbkdf2_'):
+            self.password = make_password(self.password)
+        super(UserRegister, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
---
+++ loginapi/views.py
@@ -0,0 +1,25 @@
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .models import UserRegister
+# from .serializer import UserRegisterSerializer
+
+
+# class RegisterAPI(APIView):
+
+#     def post(self,request):
+#         serializer = UserRegisterSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+from rest_framework import generics
+from .models import UserRegister
+from loginapi.serializers import UserRegisterSerializer
+
+class RegisterAPI(generics.ListCreateAPIView):
+    queryset = UserRegister.objects.all()
+    serializer_class = UserRegisterSerializer
+
+
